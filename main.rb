e48d6fc8c52d917a7173dc4f458c519baa91115f
Kate <ms.katesy@gmail.com>
2018-07-24 21:31:44 +1000

    added create new bill, plus join bill functions - work in progess

--- main.rb
+++ main.rb
@@ -17,12 +17,34 @@ helpers do
   def logged_in?
     !!current_user
   end
+
+  def random_join_id
+    ('AA000'..'ZZ999').to_a.sample
+  end
+
+    
+
 end
 
 
 get '/' do
   erb :index
 end
+
+get '/users/new' do 
+  erb :new_user
+end
+
+post '/users/new' do
+  user = User.new
+  user.name = params[:name]
+  user.email = params[:email]
+  user.password = params[:password]
+  user.save
+  session[:user_id] = user.id
+  redirect "users/#{user.id}"
+end
+
 
 get '/users/:id' do
   if logged_in?
@@ -31,7 +53,6 @@ get '/users/:id' do
     redirect '/'
   end
 end
-
 
 post '/session' do
   user = User.find_by(email: params[:email])
@@ -42,6 +63,35 @@ post '/session' do
     erb :index
   end
 end
+
+post '/bills/new' do
+  bill = Bill.new
+  bill.name = params[:name]
+  bill.user_id = current_user.id
+  bill.open_closed = 1
+  @random_num = random_join_id()
+  while Bill.find_by(join_pin: @random_num)
+    @random_num = random_join_id()
+  end 
+  bill.join_pin = @random_num
+  bill.save
+  join = Usersxbill.new
+  join.bill_id = bill.id
+  join.user_id = current_user.id
+  join.save
+  redirect "/users/#{current_user.id}"
+end
+
+post '/bills/join' do
+  bill = find_by(pin: params[:join_id])
+  join = Userxbill.new
+  join.bill_id = bill.id
+  join.user_id = current_user.id
+  join.save
+  redirect "users/#{current_user.id}"
+end
+
+
 
 delete '/session' do
   session[:user_id] = nil
--- models/bill.rb
+++ models/bill.rb
@@ -1,4 +1,7 @@
 class Bill < ActiveRecord::Base
   belongs_to :user
   has_many :items
+
+
+
  end
