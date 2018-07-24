7e0bb50f53ddd427466920941996f32bc9c405ec
Kate <ms.katesy@gmail.com>
2018-07-24 14:13:30 +1000

    updating main.rb to deploy app

--- main.rb
+++ main.rb
@@ -2,18 +2,21 @@ require 'sinatra'
 require_relative 'db_config'
 require_relative 'models/user'
 require_relative 'models/bill'
-require_relative 'models/comments'
+require_relative 'models/comment'
+require_relative 'models/item'
+require_relative 'models/usersxbill'
+
 require 'bcrypt'
 enable :sessions
 
-helpers do
-  def current_user
-    User.find_by(id: session[:user_id])
-  end
-  def logged_in?
-    !!current_user
-  end
-end
+# helpers do
+#   def current_user
+#     User.find_by(id: session[:user_id])
+#   end
+#   def logged_in?
+#     !!current_user
+#   end
+# end
 
 
 get '/' do
