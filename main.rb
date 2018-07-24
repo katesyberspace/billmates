072ccfbfef56d94a148924ed3c3b22ee0a92de1f
Kate <ms.katesy@gmail.com>
2018-07-24 14:29:50 +1000

    created login landing page and sessions

--- main.rb
+++ main.rb
@@ -5,22 +5,41 @@ require_relative 'models/bill'
 require_relative 'models/comment'
 require_relative 'models/item'
 require_relative 'models/usersxbill'
-
 require 'bcrypt'
 enable :sessions
 
-# helpers do
-#   def current_user
-#     User.find_by(id: session[:user_id])
-#   end
-#   def logged_in?
-#     !!current_user
-#   end
-# end
+helpers do
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+end
 
 
 get '/' do
   erb :index
+end
+
+get '/users/:id' do
+  erb :users_detail
+end
+
+post '/session' do
+  user = User.find_by(email: params[:email])
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect "/users/#{user.id}"
+  else
+    erb :index
+  end
+end
+
+delete '/session' do
+  session[:user_id] = nil
+  redirect '/'
 end
 
 
